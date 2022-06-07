e3c689eb274af250e3291044af8f19843df700b1
vkp3012 <vivekpatel301295@gmail.com>
2022-06-07 09:49:49 +0530

    Array is started

---
+++ datatype/array/intro.js
@@ -0,0 +1,16 @@
+/*-------------------Array------------------*/
+
+console.log("------------Declaration-----------------");
+
+//Declaration
+let arr = new Array();
+let arr1 = [];
+
+//value in array
+let fruits =["Apple","Orange","Plum"];
+
+//array element start with 0 element
+let fruits1 =["Apple","Orange","Plum"];
+console.log(fruits[0]);// Apple
+console.log(fruits[1]);// Orange
+console.log(fruits[2]);// Plum
