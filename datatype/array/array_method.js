cf931a60accfde22f273e514599edca446912cb6
vkp3012 <vivekpatel301295@gmail.com>
2022-06-07 10:48:32 +0530

    array method is done

--- datatype/array/array_method.js
+++ datatype/array/array_method.js
@@ -2,12 +2,13 @@
 /*------------------Array Method----------------------*/
 console.log("--------------Array Method-----------------");
 
-//pop
+//Methods that work with the end of the array:
+//1. pop
 let fruits =["Apple","Orange","Pear"];
 console.log(fruits.pop());// remove "Pear" and console.log it
 console.log(fruits );// Apple, Orange
 
-//push
+//2.push
 let fruits1 =["Apple","Orange"];
 fruits1.push("Pear");
 console.log(fruits1);// Apple, Orange, Pear
@@ -15,7 +16,22 @@ console.log(fruits1);// Apple, Orange, Pear
 
 //Methods that work with the beginning of the array:
 
-//shift
+//1. shift
 let fruits2 =["Apple","Orange","Pear"];
 console.log(fruits2.shift());// remove Apple and console.log it
 console.log(fruits2 );// Orange, Pear
+
+
+//2. Unshift
+let fruits3 =["Orange","Pear"];
+fruits3.unshift('Apple');
+console.log(fruits3 );// Apple, Orange, Pear
+
+
+//Methods push and unshift can add multiple elements at once:
+
+let fruits4 = ["Apple"];
+fruits4.push("Orange","Peach"); 
+console.log(fruits4);//["Apple", "Orange", "Peach"]
+fruits4.unshift("Pineapple","Lemon");
+console.log(fruits4);// ["Pineapple", "Lemon", "Apple", "Orange", "Peach"]
