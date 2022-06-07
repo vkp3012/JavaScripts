6e86f9dd54ee43232e52afb0a52f1259a4eb9d40
vkp3012 <vivekpatel301295@gmail.com>
2022-06-07 10:39:22 +0530

    array method start- pop and push are done

---
+++ datatype/array/array_method.js
@@ -0,0 +1,13 @@
+
+/*------------------Array Method----------------------*/
+console.log("--------------Array Method-----------------");
+
+//pop
+let fruits =["Apple","Orange","Pear"];
+console.log(fruits.pop());// remove "Pear" and console.log it
+console.log(fruits );// Apple, Orange
+
+//push
+let fruits1 =["Apple","Orange"];
+fruits1.push("Pear");
+console.log(fruits1);// Apple, Orange, Pear
