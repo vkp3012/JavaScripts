7556a39947ab815f06039c90c325f1710c470876
vkp3012 <vivekpatel301295@gmail.com>
2022-06-07 10:42:42 +0530

    array shift is done

--- datatype/array/array_method.js
+++ datatype/array/array_method.js
@@ -11,3 +11,11 @@ console.log(fruits );// Apple, Orange
 let fruits1 =["Apple","Orange"];
 fruits1.push("Pear");
 console.log(fruits1);// Apple, Orange, Pear
+
+
+//Methods that work with the beginning of the array:
+
+//shift
+let fruits2 =["Apple","Orange","Pear"];
+console.log(fruits2.shift());// remove Apple and console.log it
+console.log(fruits2 );// Orange, Pear
