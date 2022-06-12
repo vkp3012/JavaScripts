eca51d4767b43b71d07bf636d31832b666fc19b1
vkp3012 <vivekpatel301295@gmail.com>
2022-06-13 01:07:42 +0530

    nullish is done

---
+++ basic_js/nullish.js
@@ -0,0 +1,44 @@
+
+// result = (a !== null && a !== undefined) ? a : b;
+
+
+let user;
+console.log(user ??"Anonymous");// Anonymous (user not defined)
+
+
+let user1 ="John";
+console.log(user1 ??"Anonymous");// John (user defined)
+
+
+// let firstName =null;
+// let lastName =null; 
+// let nickName ="Supercoder";// shows the first defined value:
+// console.log(firstName ??lastName ??nickName ??"Anonymous");// Supercoder
+
+
+//Comparison with ||
+
+let firstName =null;
+let lastName =null;
+let nickName ="Supercoder";// shows the first truthy value:
+console.log(firstName ||lastName ||nickName ||"Anonymous");// Supercoder
+
+
+let height =0;
+console.log(height || 100);// 100
+console.log(height ?? 100);// 0
+
+
+//Precedence
+let height1 =null;
+let width =null;// important: use parentheses
+let area =(height1 ??100)*(width ??50);
+console.log(area);// 5000
+
+
+//Using ?? with && or ||
+// let x =1 && 2 ?? 3;// SyntaxError: Unexpected token '??'
+
+
+let x =(1&&2)??3;// Works
+console.log(x);// 2
