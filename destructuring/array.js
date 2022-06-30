f848c9a323915741ed5da37569f1ecf9af932167
vkp3012 <vivekpatel301295@gmail.com>
2022-06-30 17:41:25 +0530

    destructuring basis concept

---
+++ destructuring/array.js
@@ -0,0 +1,8 @@
+//array
+[a, b] = ["FJP", 5];
+console.log(a); //fjp
+console.log(b); //5
+
+[n1, , n3] = [1, 2, 3];
+console.log(n1);
+console.log(n3);
---
+++ destructuring/object.js
@@ -0,0 +1,8 @@
+//object destructuring
+let obj = {
+    name: "Sohana",
+    age:23
+}
+
+let { age } = obj;
+console.log(age);
