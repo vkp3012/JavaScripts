bb96b7b1f15ddf1f628b5d6c2cb9d57608a62282
vkp3012 <vivekpatel301295@gmail.com>
2022-06-30 17:29:48 +0530

    opps basic concept

---
+++ oops/inheritance.js
@@ -0,0 +1,60 @@
+//inheritance (extends keyword)
+// class LivingOrganism{
+//     constructor() {
+//         this.breaths=true;
+//     }
+
+//     isBreathing() {
+//         console.log(this.breaths);
+//     }
+// }
+
+// class Bird extends LivingOrganism{
+//     constructor() {
+//         super();
+//     this.eats = true;
+//   }
+
+//   printHabit() {
+//     console.log(this.eats);
+//   }
+// }
+
+// class Penguin extends Bird {
+//     constructor() {
+//         super(); // used to run the constructor of parent
+//     this.habitat = "Antartica";
+//   }
+
+//   printPlaceOfOrigin() {
+//     console.log(this.habitat);
+//   }
+// }
+
+// const myPenguin = new Penguin();
+// console.log(myPenguin);
+// myPenguin.printPlaceOfOrigin();
+// myPenguin.printHabit();
+
+//error-> Must call super constructor in derived class before accessing 'this' or returning from derived constructor
+
+class Bird {
+    eats = true;
+  
+    printHabit = () => {
+      console.log(this.eats);
+    };
+}
+  
+class Penguin extends Bird {
+    habitat = "Antartica";
+  
+    printPlaceOfOrigin = () => {
+      console.log(this.habitat);
+    };
+}
+  
+const myPenguin = new Penguin();
+console.log(myPenguin);
+myPenguin.printPlaceOfOrigin();
+myPenguin.printHabit();
---
+++ oops/oops.js
@@ -0,0 +1,13 @@
+class Penguin{
+    constructor() {
+        this.habitat = "Antartica";
+    }
+
+    printPlaceOfOrigin() {
+        console.log(this.habitat);
+    }
+}
+
+const myPenguin = new Penguin();
+console.log(myPenguin);
+myPenguin.printPlaceOfOrigin();
